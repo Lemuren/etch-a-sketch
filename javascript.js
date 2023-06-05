17ab48d95291b52a579e09e049fc1b0376b2814a
Lemuren <h.e.thorsell@gmail.com>
2023-06-05 02:37:18 +0200

    Remove draggability on the pixels

--- javascript.js
+++ javascript.js
@@ -14,10 +14,15 @@ function paint(e) {
         e.target.style.cssText += `background: ${chosenColor}`;
 }
 
+function chooseColor(color) {
+    chosenColor = color;
+}
+
 // Set color of each of the colors in the color-picker.
 const colorPickerButtons = document.querySelectorAll('.color-picker button');
 for (let button of colorPickerButtons) {
     button.style.cssText += `background: ${button.id}`;
+    button.addEventListener('click', e => chooseColor(e.target.id));
 }
 
 // Add a grid of pixels to the canvas.
@@ -25,9 +30,9 @@ for (let i = 0; i < (CANVAS_SIZE / pixelSize)**2; i++) {
     const canvas = document.querySelector('.canvas');
 
     let pixel = document.createElement('div');
+    pixel.setAttribute('draggable', 'false');
     pixel.style.cssText = `width: ${pixelSize}px; height: ${pixelSize}px;`;
-    pixel.addEventListener('mousedown', paint);
+    pixel.addEventListener('mousedown', e => {e.preventDefault(); paint(e)});
     pixel.addEventListener('mouseover', paint);
     canvas.append(pixel);
-
 }
