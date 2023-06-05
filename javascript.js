b619a51f168343fb4e8cbcfa7f884b6a28aef70a
Lemuren <h.e.thorsell@gmail.com>
2023-06-05 02:22:23 +0200

    Add drawing logic

--- javascript.js
+++ javascript.js
@@ -1,19 +1,23 @@
 "use strict"
-
 const CANVAS_SIZE = 640;
 
 let chosenColor = 'black';
 let pixelSize = 32;
+let isMouseDown = false;
 
-function paint(pixel) {
-    pixel.style.cssText += `background: ${chosenColor}`;
+document.body.onmousedown = () => isMouseDown = true;
+document.body.onmouseup = () => isMouseDown = false;
+
+
+function paint(e) {
+    if ((isMouseDown) || (e.type == 'mousedown'))
+        e.target.style.cssText += `background: ${chosenColor}`;
 }
 
 // Set color of each of the colors in the color-picker.
 const colorPickerButtons = document.querySelectorAll('.color-picker button');
 for (let button of colorPickerButtons) {
     button.style.cssText += `background: ${button.id}`;
-    console.log(button);
 }
 
 // Add a grid of pixels to the canvas.
@@ -22,7 +26,8 @@ for (let i = 0; i < (CANVAS_SIZE / pixelSize)**2; i++) {
 
     let pixel = document.createElement('div');
     pixel.style.cssText = `width: ${pixelSize}px; height: ${pixelSize}px;`;
-    //pixel.addEventListener('click', paint);
+    pixel.addEventListener('mousedown', paint);
+    pixel.addEventListener('mouseover', paint);
     canvas.append(pixel);
 
 }
