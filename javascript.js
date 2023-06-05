473336b3cb1e2165d5472c28c802c38ddc20d991
Lemuren <h.e.thorsell@gmail.com>
2023-06-05 02:52:41 +0200

    Implement size picker

--- javascript.js
+++ javascript.js
@@ -29,13 +29,19 @@ function createCanvas() {
 
     // Create the canvas of pixels.
     for (let i = 0; i < (CANVAS_SIZE / pixelSize)**2; i++) {
-
         let pixel = document.createElement('div');
-        pixel.setAttribute('draggable', 'false');
         pixel.style.cssText = `width: ${pixelSize}px; height: ${pixelSize}px;`;
         pixel.addEventListener('mousedown', e => {e.preventDefault(); paint(e)});
         pixel.addEventListener('mouseover', paint);
         canvas.append(pixel);
+    }
+}
+
+function setPixelSize(size) {
+    let clear = confirm('Warning: This will clear the canvas!');
+    if (clear) {
+        pixelSize = CANVAS_SIZE / size;
+        createCanvas();
     }
 }
 
@@ -46,5 +52,12 @@ for (let button of colorPickerButtons) {
     button.style.cssText += `background: ${button.id}`;
     button.addEventListener('click', e => chooseColor(e.target.id));
 }
+
+// Add event listeners to the size picking buttons.
+const sizeButtons = document.querySelectorAll('.size-picker button');
+for (let button of sizeButtons) {
+    button.addEventListener('click', e => setPixelSize(e.target.id));
+}
+
 
 createCanvas();
