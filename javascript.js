82212a0a7e38e41a614a32f53acdcc442b7e5eab
Lemuren <h.e.thorsell@gmail.com>
2023-06-05 02:42:26 +0200

    Move canvas creation into its own function

--- javascript.js
+++ javascript.js
@@ -18,6 +18,28 @@ function chooseColor(color) {
     chosenColor = color;
 }
 
+
+function createCanvas() {
+    const canvas = document.querySelector('.canvas');
+
+    // Remove any existing children.
+    while (canvas.lastChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
+
+    // Create the canvas of pixels.
+    for (let i = 0; i < (CANVAS_SIZE / pixelSize)**2; i++) {
+
+        let pixel = document.createElement('div');
+        pixel.setAttribute('draggable', 'false');
+        pixel.style.cssText = `width: ${pixelSize}px; height: ${pixelSize}px;`;
+        pixel.addEventListener('mousedown', e => {e.preventDefault(); paint(e)});
+        pixel.addEventListener('mouseover', paint);
+        canvas.append(pixel);
+    }
+}
+
+
 // Set color of each of the colors in the color-picker.
 const colorPickerButtons = document.querySelectorAll('.color-picker button');
 for (let button of colorPickerButtons) {
@@ -25,14 +47,4 @@ for (let button of colorPickerButtons) {
     button.addEventListener('click', e => chooseColor(e.target.id));
 }
 
-// Add a grid of pixels to the canvas.
-for (let i = 0; i < (CANVAS_SIZE / pixelSize)**2; i++) {
-    const canvas = document.querySelector('.canvas');
-
-    let pixel = document.createElement('div');
-    pixel.setAttribute('draggable', 'false');
-    pixel.style.cssText = `width: ${pixelSize}px; height: ${pixelSize}px;`;
-    pixel.addEventListener('mousedown', e => {e.preventDefault(); paint(e)});
-    pixel.addEventListener('mouseover', paint);
-    canvas.append(pixel);
-}
+createCanvas();
